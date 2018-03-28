9aca2b46ea9455da2bebd99cca261985c6b50a31
Brian Hernandez <brianhernandez@users.noreply.github.com>
2018-03-28 14:42:42 -0400

    Added tabs UI and basic file upload buttons.

---
+++ json-validator.js
@@ -0,0 +1,24 @@
+var fileUploadElement = document.getElementById('fileUpload');
+
+console.log("json-validator.js file has loaded.");
+
+fileUploadElement.addEventListener('change', function(event) {
+  var file = this.files[0].name;
+  // var default = this.attr("placeholder");
+  console.log(this.parentNode.firstChild);
+  this.parentNode.firstChild.innerHTML = file;
+  // console.log(this);
+  // console.log(default);
+  // console.log(default);
+});
+
+// $("[type=file]").on("change", function(){
+//   // Name of file and placeholder
+//   var file = this.files[0].name;
+//   var dflt = $(this).attr("placeholder");
+//   if($(this).val()!=""){
+//     $(this).next().text(file);
+//   } else {
+//     $(this).next().text(dflt);
+//   }
+// });
