af53bd84a8aed7b787362ba7903a302f56038725
Brian Hernandez <brianhernandez@users.noreply.github.com>
2018-04-01 21:18:06 -0400

    Added code comments to the json-validator.js file.

--- json-validator.js
+++ json-validator.js
@@ -1,105 +1,115 @@
+// Obtain both file upload button and text submit button elements and save
+// them to a variable
 var fileUploadElement = document.getElementById('fileUpload');
 var textSubmitElement = document.getElementById('textSubmit');
 
-console.log("json-validator.js file has loaded.");
-
+// Add an event listener to the file upload button...
 fileUploadElement.addEventListener('change', function(event) {
+  // Obtain the name of the file that fired the change event and save to a var
   var file = this.files[0].name;
-  // var default = this.attr("placeholder");
-  console.log(this.parentNode.firstChild);
+  // Change the text of the file upload button to the name of the file choosen
   this.parentNode.firstChild.innerHTML = file;
 });
-
+// Add an event listener to the text submit button...
 textSubmitElement.addEventListener('click', function(event) {
+  // Get the textarea value and save to a variable
   var textareaValue = document.getElementById('textarea').value;
-  // var textareaValueSplit = textareaValue.split(/\n/g);
+  // Trim the textarea value text then split into an array by carriage returns
   var textareaValueSplitArray = document.getElementById('textarea').value.trim().split(/\n/g);
+  // Get a pointer to the results tab ul element
   var resultsTabElement = document.getElementsByClassName('evo_c-markup__item');
+  // Create a div element to put the validator output in
   var codeResultContainer = document.createElement("div");
+  // Create a pre element to put entered code in correct formatting in results
   var preFormattedTextElement = document.createElement("pre");
-  // var preFormattedResultsMessageElement = document.createElement("pre");
+  // Create a code element to put entered code in an appropirate element for display
   var codeElement = document.createElement("code");
+  // Create a p element to enter validator results message
   var resultsMessageElement = document.createElement("p");
+  //  Initialize the invalid JSON message
   var invalidJSONMessage = "";
+  // Initialize the valid JSON message
   var validJSONMessage = "Your JSON code is valid.";
 
+  // Add class name to the p element for styling for validator results message
   resultsMessageElement.className = "codeResults__paragraph";
+  // Add class name to div element for style for outputted code in results tab
   codeResultContainer.className = "codeResults__code";
+  // Trim the code entered in the textarea field and set it as the inner text
+  // of the created code element
   codeElement.innerHTML = textareaValue.trim();
+  // Append the code element to the created pre element
   preFormattedTextElement.appendChild(codeElement);
+  // Append the preformatted code block to the codeResultContainer div
   codeResultContainer.appendChild(preFormattedTextElement);
-
+  // prevent the default action of the event handler
   event.preventDefault();
 
-
-  // console.log(textareaValue);
-  // console.log(textareaValueSplit);
-  console.log(textareaValueSplitArray);
-
-
-
+  // For each entry in the textareaValueSplitArray array...
   for (var i = 0; i < textareaValueSplitArray.length; i++) {
-    // console.log('textareaValueSplitArray length equals: ' + textareaValueSplitArray.length);
+    // Set the beginning JSON match pattern
     var beginJSONPattern = /^{{1}/g;
+    // Set the end JSON match pattern
     var endJSONPattern = /}{1}$/g;
+    // Set the last JSON key value pair match pattern
     var lastKeyValueJSONPattern = /\s*"[\w-$]+":\s{1}"[\w\s,]+"/g;
+    // Set every other JSON key value pair match patter not at the end
     var notLastKeyValueJSONPattern = /\s*"[\w-$]+":\s{1}"[\w\s,]+",/g;
 
-
-
+    // If i is at the beginning of the entered code block...
     if (i === 0) {
-      console.log(textareaValueSplitArray[i]);
-      console.log(typeof(textareaValueSplitArray[i]));
-      // console.log('i is at the beginning at: ' + i);
+      // ...if the beginning JSON pattern is NOT matched...
       if (!textareaValueSplitArray[i].match(beginJSONPattern)) {
+        // ...add the first line error message to invalidJSONMessage variable...
         invalidJSONMessage += "Code inputted is invalid at line 1.\n";
-        // console.log("Code inputted is invalid at line 1.");
       }
+      // ...else if i is at the last line of the entered code block...
     } else if (i === textareaValueSplitArray.length - 1) {
+      // ...if last line JSON pattern is NOT matched...
       if (!textareaValueSplitArray[i].match(endJSONPattern)) {
+        // ...add the last line invalid error message to invalidJSONMessage
         invalidJSONMessage += "Code inputted is invalid at last line.\n";
-        // console.log("Code inputted is invalid at last line.");
       }
-      // console.log('i is at the ends at: ' + i);
-
+      // ...else if i is at the last key value pair line...
     } else if (i === textareaValueSplitArray.length - 2) {
+      // ...if the last key value pair JSON pattern is NOT matched...
       if (!textareaValueSplitArray[i].match(lastKeyValueJSONPattern)) {
+        // ...add the invalid error message with the appropriate line number in
+        // the code
         invalidJSONMessage += "Code inputted is invalid at line " + (i + 1) + ".\n";
-        // console.log("Code inputted is invalid at line " + i + ".");
       }
-      // console.log('i is at the second to the last line at: ' + i);
-
+      // ...else if i is neither at the end, or beginning of the code block...
     } else if ( i >= 1 && i <= textareaValueSplitArray.length) {
+      // ...if the current i position in the array does NOT match the key value
+      // pair JSON match pattern...
       if (!textareaValueSplitArray[i].match(notLastKeyValueJSONPattern)) {
+        // ...add the invalid error message along with the appropriate human
+        // readable line number
         invalidJSONMessage += "Code inputted is invalid at line " + (i + 1) + ".\n";
-        // console.log("Code inputted is invalid at line " + i + ".");
       }
-      // console.log(' is iN THE MIDDLE at: ' + i);
     }
   }
-  console.log(invalidJSONMessage);
-
+  // If the invalidJSONMessage is empty...
   if (invalidJSONMessage === "") {
+    // ...set the p resultsMessageElement to valid styling via a class
     resultsMessageElement.classList.add("codeResults__paragraph--valid");
+    // JSON is valid, set the appropriate valid message to the p element
     resultsMessageElement.innerText = validJSONMessage;
-    // preFormattedResultsMessageElement.appendChild(resultsMessageElement);
+    // ...else...
   } else {
+    // ...set the p resultsMessageElement to invalid styling via a class
     resultsMessageElement.classList.add("codeResults__paragraph--invalid");
+    // JSON is invalid, set the appropriate invalid message(s) to the p element
     resultsMessageElement.innerText = invalidJSONMessage;
-    // preFormattedResultsMessageElement.appendChild(resultsMessageElement);
   }
-  // console.log(preFormattedResultsMessageElement);
-  // console.log(resultsTabElement[2]);
-  console.log(document.getElementsByClassName('evo_c-markup__content')[2]);
-  // document.getElementsByClassName('evo_c-markup__content')[2].removeChild("p");
+  // Get the contents of the results tab content area and set it to an empty string
   document.getElementsByClassName('evo_c-markup__content')[2].innerHTML = "";
+  // Append the codeResultsContainer div and its formatted code content to the
+  // results tab
   document.getElementsByClassName('evo_c-markup__content')[2].appendChild(codeResultContainer);
+  // Append the resultsMessageElement p and its message contents to the results tab
   document.getElementsByClassName('evo_c-markup__content')[2].appendChild(resultsMessageElement);
-
+  // Initiate a click event on the results tab to bring it in focus and its contents
+  // visible
   document.getElementsByClassName('evo_c-markup__item')[2].click();
-
-  console.log(codeResultContainer);
-
-
-
 });
